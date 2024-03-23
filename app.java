5faf0bc2a054a4b7eee0c7a1139894e246eb923f
Ashley <aquareos21@gmail.com>
2024-03-23 15:02:44 -0500

    added some example images

--- app.java
+++ app.java
@@ -5,14 +5,16 @@ public class app {
         JFrame window = new JFrame("recyclables"); // set title for window
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // stop app when window is closed
 
-        window.setResizable(true); // dont allow user to resize window
-        window.setSize(400, 300); // set initial size of the window
+        window.setResizable(true); // allow user to resize window
+        window.pack();
+        window.setLocationRelativeTo(null); // window appears in the middle of the screen
+        window.setSize(400,300);
+        
         window.setVisible(true); // this makes the window show up 
     }
 
     // call the method :D 
     public static void main(String[] args) {
-        initWindow(); // call the method to initialize the window
-        
+        initWindow(); // call the method to show the window
     }
 }
