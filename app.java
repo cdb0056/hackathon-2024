fe9dfce78226aeb594644631e6e00c5b9f499a87
Katan Jones <katanj123@yahoo.com>
2024-03-23 21:39:40 -0500

    gfhgf

--- app.java
+++ app.java
@@ -7,10 +7,10 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.event.*;
 
-public class app {
+public class app { 
     private static JFrame window;
-    private static ActionListener listener;
-
+    private static ActionListener listener;  
+   
     private static void initWindow() { // make window for the game
         JFrame window = new JFrame("recyclables"); // set title for window
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // stop app when window is closed
